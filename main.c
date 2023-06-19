340229c75edb2f1f6ddd7c9231f91acf5a943b1e
pierreantoinesoyer <pierreantoine.soyer@reseau.eseo.fr>
2023-06-19 11:18:27 +0200

    main edited

--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "interfaceUtilisateur.h"
+#include "dictionnaire.h"
+#include "process.h"
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int MAX_SIZE = 25;
+
+    bool keepPlaying = true;
+
+    while(keepPlaying==true)
+    {
+        int erreurs = 0;
+        int partie = 0;
+        int size = 0;
+
+        char* mot = demandeMot();
+
+        while (mot[size] != '\0') {
+            size++;
+        }
+
+
+        char pendu[size] = {'_'};
+
+        while(partie == 0)
+        {
+            char lettre = demandeLettre();
+            partie = compareMot(lettre, mot, &pendu, &erreurs, size);
+            afficherPendu(&pendu, erreurs, partie, size);
+        }
+        //bool keepPlaying = demandeContinuer();
+    }
+        return 0;
 }
