fd4f41f35d9b0b49bd3fc100c0ccfd821b6b4a76
pierreantoinesoyer <pierreantoine.soyer@reseau.eseo.fr>
2023-06-19 16:31:01 +0200

    main edited

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 #include "interfaceUtilisateur.h"
 #include "dictionnaire/dictionnaire.h"
-#include "process.h"
+#include "check.h"
 
 int main()
 {
@@ -39,7 +39,7 @@ int main()
         while(partie == 0)
         {
             char lettre = demandeLettre();   //demande une lettre à l'utilisateur
-            partie = compareMot(lettre, mot, pendu, &erreurs, size);   //modifie le nombre d'erreurs, et le pendu accordément
+            partie = compareMot(mot, pendu, &erreur);   //modifie le nombre d'erreurs, et le pendu accordément
             afficherPendu(pendu, erreurs, partie);  //affiche les informations nécessaires au joueur
         }
         free(pendu);
