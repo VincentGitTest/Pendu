c74bc91a7f39d6d9bebc8b124f63de668ba90a63
Belmont CLAIRE <belmont.claire1@gmail.com>
2023-06-19 12:23:53 +0200

    Ajout interface utilisateur

--- interfaceUtilisateur.h
+++ interfaceUtilisateur.h
@@ -1,31 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#ifndef INTERFACEUTILISATEUR_H_DEFINI_
+#define INTERFACEUTILISATEUR_H_DEFINI_
 
-void affichePendul(char pendu[], int erreur, int partie)
-{
-    if(partie == 0)
-    {
-        printf("La partie continue :\n");
-        printf("Vous avez fait %d erreur(s).\n",erreur);
-        printf("Voici le mot avec les lettres qui manquent : \n");
-        printf("%s\n",pendu);
-    }
-    else if (partie == -1)
-    {
-        printf("Defaite : tu as fait trop d'erreurs.\n");
-    }
-    else if (partie == 1)
-    {
-        printf("Victoire : tu as trouve le mot.\n");
-    }
-}
+void affichePendul(char pendu[], int erreurs, int partie);
+char demandeLettre(void);
 
-char demandeLettre()
-{
-    char lettre;
-    printf("Donner moi une lettre : \n");
-    scanf("%c", &lettre);
-    return lettre;
-}
-
+#endif
